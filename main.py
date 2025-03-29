768344163e5beb9a45666d29e87f9931e2d45f49
kalmk <111930167+kalmk@users.noreply.github.com>
2025-03-28 17:15:03 -0700

    added debug_print() to change accordingly

--- main.py
+++ main.py
@@ -2,29 +2,37 @@ from get_tokens import *
 from data import *
 from tree import *
 
+# Change this from True/False to enable/disable debug log
+DEBUG = True
+
+
+def debug_print(message):
+    if DEBUG:
+        print(message)
+
 
 def main():
-    input = get_tokens("./test_cases/6.in")
+    input = get_tokens("./test_cases/1.in")
 
     if not input:
         return
 
     input.append("$")
-    print(input)
+    debug_print(input)
 
     stack = [0]
     cst_stack = []
     input_index = 0
 
-    print(f"Beginning Stack: {stack}")
+    debug_print(f"Beginning Stack: {stack}")
 
     while True:
         current_state = stack[-1]
         current_token = input[input_index]
-        print(f"Current token: {current_token}")
+        debug_print(f"Current token: {current_token}")
 
         action = table[current_state][table_column.index(current_token)]
-        print(f"Action: {action}")
+        debug_print(f"Action: {action}")
 
         if action is None:
             print("----------ERROR----------")
@@ -44,12 +52,12 @@ def main():
             cst_stack.append(tree_node(current_token))
 
             input_index += 1
-            print(f"Shifted Stack: {stack}")
+            debug_print(f"Shifted Stack: {stack}")
 
         elif action.startswith('R'):
             production_index = int(action[1:])
             lhs, rhs = productions[production_index]
-            print(
+            debug_print(
                 f"Reducing using rule {production_index}: {lhs} -> {' '.join(rhs)}")
 
             # Pop twice for each symbol in RHS (symbol + state)
@@ -61,7 +69,7 @@ def main():
             goto_state = table[new_state][table_column.index(lhs)]
             stack.append(lhs)
             stack.append(int(goto_state))
-            print(f"Reduced Stack: {stack}")
+            debug_print(f"Reduced Stack: {stack}")
 
             # Create CST node
             num_children = len(rhs)
@@ -72,8 +80,8 @@ def main():
             cst_stack.append(new_node)
 
         elif action == "Acc":
-            print("Parsing successful. Accepting input.\n")
-            print("Concrete Syntax Tree:")
+            debug_print("Parsing successful. Accepting input.\n")
+            debug_print("Concrete Syntax Tree:")
             if cst_stack:
                 cst_stack[0].print_tree()
             else:
